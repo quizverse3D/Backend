6056e1952209088a08b5c7f8bab67d107355f5ae
Igor Stepanov <istepanov@lanit.ru>
2025-09-07 15:06:28 +0300

    Rooms Swagger: описание метода, поправил constraint в структуре БД

--- scripts/room.sql
+++ scripts/room.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS rooms (
     owner_id UUID NOT NULL,
     name VARCHAR(128) NOT NULL,
     password_hash TEXT,
-    max_players INT NOT NULL CHECK (max_players > 0 and max_players < 32),
+    max_players INT NOT NULL CHECK (max_players > 0 and max_players <= 32),
     created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
     is_public BOOLEAN NOT NULL DEFAULT true
 );
